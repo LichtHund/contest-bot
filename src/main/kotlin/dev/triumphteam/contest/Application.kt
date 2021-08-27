a65721fed85769fc915bd784b63dca9269c5cd31
Matt <xpsykogfx@gmail.com>
2021-08-27 01:53:46 +0100

    Fixed slash commands not showing

--- src/main/kotlin/dev/triumphteam/contest/Application.kt
+++ src/main/kotlin/dev/triumphteam/contest/Application.kt
@@ -3,6 +3,7 @@ package dev.triumphteam.contest
 import dev.triumphteam.contest.func.tokenFromFlag
 import dev.triumphteam.jda.JdaApplication
 import dev.triumphteam.jda.jda
+import net.dv8tion.jda.api.events.guild.GuildAvailableEvent
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.OptionType
@@ -28,38 +29,5 @@ fun main(args: Array<String>) {
         applicationFolder = File("data"),
     ) {
         disableCache(CacheFlag.ACTIVITY)
-        addEventListeners(
-            // Had to move this here because it's called before everything else
-            object : ListenerAdapter() {
-                override fun onGuildReady(event: GuildReadyEvent) {
-                    val guild = event.guild
-
-                    // Upsert all commands
-                    guild.upsertCommand(
-                        CommandData("accept", "Accept a team invite").apply {
-                            addOption(OptionType.USER, "inviter", "Member that invited you", true)
-                        }
-                    ).queue()
-                    guild.upsertCommand(
-                        CommandData("invite", "Invite a partner").apply {
-                            addOption(OptionType.USER, "partner", "Partner you want to invite", true)
-                        }
-                    ).queue()
-                    guild.upsertCommand(
-                        CommandData("team", "Information about your team").apply {
-                            addOption(OptionType.USER, "member", "Checks the member's team information")
-                        }
-                    ).queue()
-                    guild.upsertCommand(
-                        CommandData("participate", "Sign up for the contest").apply {
-                            addOption(OptionType.STRING, "repo", "GitHub repository for the contest", true)
-                            addOption(OptionType.USER, "partner", "Teams of 2 are allowed, so introduce your partner")
-                        }
-                    ).queue()
-
-                    guild.updateCommands().queue()
-                }
-            }
-        )
     }
 }
--- src/main/kotlin/dev/triumphteam/contest/Module.kt
+++ src/main/kotlin/dev/triumphteam/contest/Module.kt
@@ -14,7 +14,8 @@ import dev.triumphteam.contest.listeners.voting
 import dev.triumphteam.jda.JdaApplication
 import dev.triumphteam.contest.event.listen
 import net.dv8tion.jda.api.entities.Activity
-import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
 
 fun JdaApplication.module() {
     val config = install(Config)
@@ -30,6 +31,38 @@ fun JdaApplication.module() {
     // Actual listener
     listen(JdaApplication::voting)
 
-    jda.presence.setPresence(Activity.competing("the Plugin Jam!"), false)
+    jda.presence.setPresence(Activity.competing("in Plugin Jam!"), false)
+    upsertCommands()
+}
 
+fun JdaApplication.upsertCommands() {
+    jda.guilds.forEach { guild ->
+
+        println("Upserting")
+        // Upsert all commands
+        guild.upsertCommand(
+            CommandData("accept", "Accept a team invite").apply {
+                addOption(OptionType.USER, "inviter", "Member that invited you", true)
+            }
+        ).queue()
+        guild.upsertCommand(
+            CommandData("invite", "Invite a partner").apply {
+                addOption(OptionType.USER, "partner", "Partner you want to invite", true)
+            }
+        ).queue()
+        guild.upsertCommand(
+            CommandData("team", "Information about your team").apply {
+                addOption(OptionType.USER, "member", "Checks the member's team information")
+            }
+        ).queue()
+        guild.upsertCommand(
+            CommandData("participate", "Sign up for the contest").apply {
+                addOption(OptionType.STRING, "repo", "GitHub repository for the contest", true)
+                addOption(OptionType.USER, "partner", "Teams of 2 are allowed, so introduce your partner")
+            }
+        ).queue()
+
+        guild.updateCommands().queue()
+        println("done")
+    }
 }
